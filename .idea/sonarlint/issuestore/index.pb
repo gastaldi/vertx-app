07b5eea5a78ff4076179bcb420c6d8feeda913c2
George Gastaldi <gegastaldi@gmail.com>
2018-11-13 13:48:29 +0000

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+m
+=src/main/java/io/openshift/booster/database/DBInitHelper.java,7/f/7f9727a6e5c6a959ef6a5902e5b71d870371433a
+g
+7src/main/java/io/openshift/booster/database/Errors.java,4/9/491787cecc541892b0f2c3aa446b5bb07ca02e1d
+p
+@src/main/java/io/openshift/booster/database/CrudApplication.java,f/f/ffa8b8f7438aa68698eb5601933f75f5f45c3270
+~
+Nsrc/main/java/io/openshift/booster/database/service/impl/JdbcProductStore.java,e/7/e74b3455278ab0dcc6ce554c98b4e16cc6c30637
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+\
+,src/main/java/io/openshift/booster/Main.java,0/c/0cf0b3a53cd72ba4d9e1e6d220304ce8df9fb969
---
+++ src/main/java/io/openshift/booster/MainApplication.java
@@ -0,0 +1,65 @@
+package io.openshift.booster;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import io.openshift.booster.database.CrudApplication;
+import io.openshift.booster.http.HttpApplication;
+import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.ext.web.Router;
+import io.vertx.rxjava.ext.web.handler.BodyHandler;
+import io.vertx.rxjava.ext.web.handler.StaticHandler;
+import rx.Completable;
+
+/**
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+public class MainApplication extends AbstractVerticle {
+
+  @Override
+  public void start() {
+    // Create Router
+    Router router = createRouter();
+
+    List<Completable> futures = new ArrayList<>();
+
+    // Initialize all Router Consumers
+    for (RouterConsumer consumer : getRouterConsumers()) {
+      consumer.accept(router);
+      futures.add(consumer.start());
+    }
+
+    Completable.concat(futures)
+      .subscribe(() ->
+                   vertx.createHttpServer()
+                     .requestHandler(router::accept)
+                     .rxListen(config().getInteger("http.port", 8080))
+                     .subscribe(httpServer ->
+                                  System.out.println("Server started on port " + httpServer.actualPort()))
+      );
+  }
+
+  private Router createRouter() {
+    // Create a router object.
+    Router router = Router.router(vertx);
+    // enable parsing of request bodies
+    router.route().handler(BodyHandler.create());
+
+    // health check
+    router.get("/health").handler(rc -> rc.response().end("OK"));
+
+    // web interface
+    router.get().handler(StaticHandler.create());
+    return router;
+  }
+
+
+  private Collection<RouterConsumer> getRouterConsumers() {
+    return Arrays.asList(
+      new HttpApplication(vertx),
+      new CrudApplication(vertx)
+    );
+  }
+}

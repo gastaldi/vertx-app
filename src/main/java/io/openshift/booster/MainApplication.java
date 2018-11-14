134f110ae6bfd5ee74fd2e13d36564958287e5f4
Clement Escoffier <clement.escoffier@gmail.com>
2018-11-14 10:58:07 +0100

    update to rxjava 2, refactoring

--- src/main/java/io/openshift/booster/MainApplication.java
+++ src/main/java/io/openshift/booster/MainApplication.java
@@ -1,43 +1,56 @@
 package io.openshift.booster;
 
-import java.util.Objects;
-
 import io.openshift.booster.database.CrudApplication;
 import io.openshift.booster.http.HttpApplication;
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
 import io.vertx.core.Future;
-import io.vertx.rxjava.core.AbstractVerticle;
-import io.vertx.rxjava.ext.web.Router;
-import io.vertx.rxjava.ext.web.handler.BodyHandler;
-import io.vertx.rxjava.ext.web.handler.StaticHandler;
-import rx.Observable;
+import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.handler.BodyHandler;
+import io.vertx.reactivex.ext.web.handler.StaticHandler;
+
 
 /**
  * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
  */
 public class MainApplication extends AbstractVerticle {
 
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(MainApplication.class.getName());
+  }
+
   @Override
-  public void start(final Future future) {
+  public void start(final Future<Void> future) {
     // Create Router
     Router router = createRouter();
 
-    Observable.from(getRouterConsumers())
-      .filter(Objects::nonNull)
-      .flatMapCompletable(r -> {
-        r.accept(router);
-        return r.start();
-      })
-      .toCompletable()
-      .doOnCompleted(() -> future.complete())
-      .doOnError(throwable -> future.fail(throwable))
-      .subscribe(() ->
-                   vertx.createHttpServer()
-                     .requestHandler(router::accept)
-                     .rxListen(config().getInteger("http.port", 8080))
-                     .subscribe(httpServer ->
-                                  System.out.println("Server started on port " + httpServer.actualPort()))
+    Completable routes = initRoutes(router);
+    Completable http = initHttpServer(router);
 
+    //noinspection ResultOfMethodCallIgnored
+    routes
+      .andThen(http)
+      .subscribe(
+        future::complete,
+        future::fail
       );
+  }
+
+  private Completable initRoutes(Router router) {
+    return Flowable.fromArray(getRouterConsumers())
+      .doOnNext(consumer -> consumer.accept(router))
+      .flatMapCompletable(RouterConsumer::start);
+  }
+
+  private Completable initHttpServer(Router router) {
+    return vertx.createHttpServer()
+      .requestHandler(router::accept)
+      .rxListen(config().getInteger("http.port", 8080))
+      .doOnSuccess(server -> System.out.println("Server started on port " + server.actualPort()))
+      .toCompletable();
   }
 
   private Router createRouter() {
@@ -50,7 +63,7 @@ public class MainApplication extends AbstractVerticle {
     router.get("/health").handler(rc -> rc.response().end("OK"));
 
     // web interface
-    router.get().handler(StaticHandler.create());
+    router.get().last().handler(StaticHandler.create());
     return router;
   }
 
@@ -59,8 +72,6 @@ public class MainApplication extends AbstractVerticle {
       // TODO: Add new RouteConsumers here
       new CrudApplication(vertx),
       new HttpApplication(vertx),
-      // This is to ease the code generation
-      null
     };
   }
 }
--- src/main/java/io/openshift/booster/http/HttpApplication.java
+++ src/main/java/io/openshift/booster/http/HttpApplication.java
@@ -3,11 +3,12 @@ package io.openshift.booster.http;
 import java.util.Objects;
 
 import io.openshift.booster.RouterConsumer;
+import io.reactivex.Completable;
 import io.vertx.core.json.JsonObject;
-import io.vertx.rxjava.core.Vertx;
-import io.vertx.rxjava.ext.web.Router;
-import io.vertx.rxjava.ext.web.RoutingContext;
-import rx.Completable;
+import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
+
 
 import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
@@ -17,23 +18,18 @@ public class HttpApplication extends RouterConsumer {
     super(vertx);
   }
 
-  private static final String template = "Hello, %s!";
+  private static final String TEMPLATE = "Hello, %s!";
 
   @Override
   public void accept(Router router) {
     router.get("/api/greeting").handler(this::greeting);
   }
 
-  @Override
-  public Completable start() {
-    return Completable.complete();
-  }
-
   private void greeting(RoutingContext rc) {
     String name = Objects.toString(rc.request().getParam("name"), "World");
 
     JsonObject response = new JsonObject()
-      .put("content", String.format(template, name));
+      .put("content", String.format(TEMPLATE, name));
 
     rc.response()
       .putHeader(CONTENT_TYPE.toString(), "application/json; charset=utf-8")
